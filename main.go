897fcf5fa1ceb281ef75e6a0a777f8f0c068258a
AV Services <av-scheduling-dev@byu.edu>
2018-03-24 18:09:23 -0600

    Fleshed out partial and full alarms

--- main.go
+++ main.go
@@ -3,15 +3,58 @@ package main
 import (
 	"log"
 	"os/exec"
+	"regexp"
+	"strconv"
+
+	"github.com/bluele/slack"
 )
 
-func main() {
-	output, err := exec.Command("vcgencmd", " measure_temp").Output()
+const (
+	webhook = "webHook"
+)
 
+func getTemp() float64 {
+	reg, err := regexp.Compile("[^0-9.]+")
 	if err != nil {
-		log.Printf(err.Error())
+		log.Fatal(err)
 	}
-	if len(output) > 0 {
-		log.Printf("output: %s", output)
+	output, execErr := exec.Command("vcgencmd", " measure_temp").Output()
+
+	if execErr != nil {
+		log.Fatal(err.Error())
+	}
+	tmp := reg.ReplaceAllString(string(output[:]), "")
+	f, _ := strconv.ParseFloat(tmp, 64)
+	return f
+}
+
+func partialAlarm(t float64) {
+	if (t >= 80.0) && (t <= 85.0) {
+		hook := slack.NewWebHook(webhook)
+		err := hook.PostMessage(&slack.WebHookPostPayload{
+			Text: "Medium-High Temp Alarm on processor",
+		})
+		if err != nil {
+			panic(err)
+		}
 	}
 }
+
+func fullAlarm(t float64) {
+	if t > 85.0 {
+		hook := slack.NewWebHook(webhook)
+		err := hook.PostMessage(&slack.WebHookPostPayload{
+			Text: "Medium-High Temp Alarm on processor",
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	output := getTemp()
+	partialAlarm(output)
+	fullAlarm(output)
+}
+
